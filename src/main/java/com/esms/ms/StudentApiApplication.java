b42fac657abf2abf8da9816430e99d057554d226
CloudAppsDeveloper <developer.cloudapps@gmail.com>
2021-11-13 16:21:08 +0530

    app configuration changes
    app configuration changes

--- src/main/java/com/esms/ms/StudentApiApplication.java
+++ src/main/java/com/esms/ms/StudentApiApplication.java
@@ -2,8 +2,10 @@ package com.esms.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class StudentApiApplication {
 
 	public static void main(String[] args) {
